3307e7eb9246a29d74a62401d6e2ddd2b12f2869
Roland Schlaefli <rolandschlaefli@gmail.com>
2020-04-10 10:24:02 +0200

    Update manifest paths for submodules

--- scripts/messaging-linux.sh
+++ scripts/messaging-linux.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+mkdir -p ~/.mozilla/native-messaging-hosts ~/.config/google-chrome/NativeMessagingHosts ~/.config/chromium/NativeMessagingHosts
 cp -u manifest-firefox.json ~/.mozilla/native-messaging-hosts/tabs.json
 cp -u manifest-chrome.json ~/.config/google-chrome/NativeMessagingHosts/tabs.json
 cp -u manifest-chrome.json ~/.config/chromium/NativeMessagingHosts/tabs.json

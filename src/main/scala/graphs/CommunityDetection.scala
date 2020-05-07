b68b97bdda078ae6bb035c31623d8b3b41be4883
Roland Schlaefli <rolandschlaefli@gmail.com>
2020-05-07 22:47:41 +0200

    Current progress with java graph lib for clustering

--- src/main/scala/graphs/CommunityDetection.scala
+++ src/main/scala/graphs/CommunityDetection.scala
@@ -4,6 +4,9 @@ import scalax.collection.Graph
 import scalax.collection.edge.WDiEdge
 import scalax.collection.io.dot._
 import scalax.collection.edge.Implicits._
+import org.jgrapht.graph.DefaultWeightedEdge
+import org.jgrapht.graph.SimpleWeightedGraph
+import org.nlpub.watset.graph.SimplifiedWatset
 
 import tabstate.Tabs
 import tabstate.Tab
@@ -11,7 +14,24 @@ import heuristics.TabSwitches
 import com.typesafe.scalalogging.LazyLogging
 
 object CommunityDetection extends App with LazyLogging {
+  val watsetGraph =
+    SimpleWeightedGraph.createBuilder[Tabs, DefaultWeightedEdge](
+      classOf[DefaultWeightedEdge]
+    )
+
   def processGraph(graph: Graph[Tabs, WDiEdge]) = {
     logger.info("> processing the graph")
+
+    // graph.nodes.foreach(node => {
+    //   watsetGraph.addVertex(node.value)
+    // })
+
+    // graph.edges.foreach(edge => {
+    //   watsetGraph.addEdge(edge.from.value, edge.to.value, edge.weight)
+    // })
+
+    // watsetGraph.build()
+
+    // logger.info(watsetGraph.toString())
   }
 }

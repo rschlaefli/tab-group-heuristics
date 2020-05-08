141e4bacdcf7bf7c6cdc4a00ec066ebca77b7066
Roland Schlaefli <rolandschlaefli@gmail.com>
2020-05-08 13:33:56 +0200

    Implement basic clustering with submission to the web extension

--- src/main/scala/heuristics/HeuristicsEngine.scala
+++ src/main/scala/heuristics/HeuristicsEngine.scala
@@ -3,37 +3,28 @@ package heuristics
 import com.typesafe.scalalogging.LazyLogging
 import scalax.collection.mutable.Graph
 import scalax.collection.edge.WDiEdge
+import io.circe._, io.circe.parser._, io.circe.generic.semiauto._,
+io.circe.syntax._
 
 import tabstate._
-import graphs.CommunityDetection
+import messaging.NativeMessaging
+import main.Main
+import messaging.HeuristicsAction
 
 object HeuristicsEngine extends LazyLogging {
   def apply(): Thread = {
     val thread = new Thread {
       logger.info("> Starting to observe current tab state")
       while (true) {
-        // logger.debug(
-        //   s"> Current tab state: ${TabState.activeTab}, ${TabState.activeWindow}, ${TabState.currentTabs}, ${TabSwitches.tabOriginGraph}"
-        // )
-
-        // logger.info(
-        //   s"> Strong components: ${TabSwitches.tabBaseGraph.strongComponentTraverser().map(_.toString())}"
-        // )
-
         val cleanTabSwitchGraph = TabSwitches.cleanupGraph(TabSwitches.tabGraph)
 
-        // TabSwitches
-        //   .extractStrongComponents(
-        //     cleanTabSwitchGraph
-        //   )
-        //   .map(TabSwitches.toDotString(_))
-        //   .foreach(comp =>
-        //     logger.debug(
-        //       s">> Strong component: ${comp}"
-        //     )
-        //   )
+        val markovClusters = Watset(cleanTabSwitchGraph)
 
-        CommunityDetection.processGraph(cleanTabSwitchGraph)
+        // update the markov clusters stored in the webextension
+        NativeMessaging.writeNativeMessage(
+          Main.out,
+          HeuristicsAction("UPDATE_GROUPS", markovClusters.asJson)
+        )
 
         Thread.sleep(30000)
       }
--- src/main/scala/messaging/HeuristicsAction.scala
+++ src/main/scala/messaging/HeuristicsAction.scala
@@ -2,9 +2,23 @@ package messaging
 
 import io.circe._, io.circe.parser._, io.circe.generic.semiauto._
 
+import tabstate.Tab
+
 case class HeuristicsAction(action: String, payload: Json)
 
 object HeuristicsAction {
-  implicit val heuristicsActionEncoder: Encoder[HeuristicsAction] =
-    deriveEncoder
+  implicit val encoder: Encoder[HeuristicsAction] = deriveEncoder
 }
+
+// case class QueryTabsAction() extends HeuristicsAction("QUERY_TABS", Json.Null)
+
+// object QueryTabsAction {
+//   implicit val encoder: Encoder[QueryTabsAction] = deriveEncoder
+// }
+
+// case class GroupUpdateAction(payload: Json)
+//     extends HeuristicsAction("UPDATE_GROUPS", payload)
+
+// object GroupUpdateAction {
+//   implicit val encoder: Encoder[GroupUpdateAction] = deriveEncoder
+// }

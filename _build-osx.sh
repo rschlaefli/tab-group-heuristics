f5f12458f8b500cac0d74a7a3a4ab873552a7298
Roland Schlaefli <rolandschlaefli@gmail.com>
2020-05-18 21:38:32 +0200

    Add installer scripts for development

---
+++ _build-osx.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+sbt universal:packageOsxDmg
---
+++ _install-stage.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+mkdir -p ~/.mozilla/native-messaging-hosts ~/.config/google-chrome/NativeMessagingHosts ~/.config/chromium/NativeMessagingHosts
+cp manifest-firefox.json ~/.mozilla/native-messaging-hosts/tabs.json
+cp manifest-chrome.json ~/.config/google-chrome/NativeMessagingHosts/tabs.json
+cp manifest-chrome.json ~/.config/chromium/NativeMessagingHosts/tabs.json
+
+ln -sf /home/roland/bin/tabs/bin/*.log ./target/universal/stage/bin/
+ln -sf /home/roland/bin/tabs/bin/*.json ./target/universal/stage/bin/
+ln -sf /home/roland/bin/tabs/bin/*.dot ./target/universal/stage/bin/

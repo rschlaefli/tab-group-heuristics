932c30d50b91aa695ab48a35be6c8d1ccc8870bf
Roland Schlaefli <rolandschlaefli@gmail.com>
2020-05-18 20:38:39 +0200

    Remove outdated messaging install scripts

--- deployment/messaging-linux.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-mkdir -p ~/.mozilla/native-messaging-hosts ~/.config/google-chrome/NativeMessagingHosts ~/.config/chromium/NativeMessagingHosts
-cp -u manifest-firefox.json ~/.mozilla/native-messaging-hosts/tabs.json
-cp -u manifest-chrome.json ~/.config/google-chrome/NativeMessagingHosts/tabs.json
-cp -u manifest-chrome.json ~/.config/chromium/NativeMessagingHosts/tabs.json
--- deployment/messaging-osx.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-cp -u manifest-firefox.json ~/Library/Application Support/Mozilla/NativeMessagingHosts/automated-tab-organization.json
-cp -u manifest-chrome.json ~/Library/Application Support/Google/Chrome/NativeMessagingHosts/automated-tab-organization.json
-cp -u manifest-chrome.json ~/Library/Application Support/Chromium/NativeMessagingHosts/automated-tab-organization.json

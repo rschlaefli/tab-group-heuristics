9334a8ce2995af2047b5624f5d4f4c352cc4c7ab
Roland Schläfli <rolandschlaefli@gmail.com>
2020-05-28 12:19:51 +0200

    Add commented firefox manifest command to mac install script

--- deployment/install-mac.sh
+++ deployment/install-mac.sh
@@ -9,6 +9,7 @@ envsubst < manifest-firefox.json > manifest-firefox.json
 envsubst < manifest-chrome.json > manifest-chrome.json
 
 mkdir -p ~/Library/Application\ Support/Mozilla/NativeMessagingHosts ~/Library/Application\ Support/Google/Chrome/NativeMessagingHosts ~/Library/Application\ Support/Chromium/NativeMessagingHosts
+# cp manifest-firefox.json ~/.mozilla/native-messaging-hosts/tabs.json
 # just copy as there is no -u flag on mac!
 cp manifest-chrome.json ~/Library/Application\ Support/Google/Chrome/NativeMessagingHosts/tabs.json
 cp manifest-chrome.json ~/Library/Application\ Support/Chromium/NativeMessagingHosts/tabs.json

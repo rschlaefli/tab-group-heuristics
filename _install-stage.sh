e6dcfc938a2f7932829b14f4f0c4804ed6b2043b
Roland Schläfli <rolandschlaefli@gmail.com>
2020-06-02 09:21:15 +0200

    Fix: Staging installation breaks firefox path

--- _install-stage.sh
+++ _install-stage.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 mkdir -p ~/.mozilla/native-messaging-hosts ~/.config/google-chrome/NativeMessagingHosts ~/.config/chromium/NativeMessagingHosts
-cp deployment/manifest-firefox.json.template ~/.mozilla/native-messaging-hosts/tabs.json
+cp manifest-firefox-stage.json ~/.mozilla/native-messaging-hosts/tabs.json
 cp manifest-chrome-stage.json ~/.config/google-chrome/NativeMessagingHosts/tabs.json
 cp manifest-chrome-stage.json ~/.config/chromium/NativeMessagingHosts/tabs.json

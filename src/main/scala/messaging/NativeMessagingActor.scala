0b04fc3e523defb7cdf2c8154dbf33041486ef64
Roland Schläfli <rolandschlaefli@gmail.com>
2020-06-18 19:09:36 +0200

    Add some initial actors

---
+++ src/main/scala/messaging/NativeMessagingActor.scala
@@ -0,0 +1,12 @@
+package messaging
+
+import akka.actor.Actor
+import akka.actor.ActorLogging
+
+class NativeMessagingActor extends Actor with ActorLogging {
+
+  override def receive: Actor.Receive = ???
+
+}
+
+object NativeMessagingActor {}
---
+++ src/main/scala/statistics/StatisticsActor.scala
@@ -0,0 +1,31 @@
+package statistics
+
+import scala.collection.mutable
+import akka.actor.Actor
+import akka.actor.ActorLogging
+
+import tabstate.Tab
+import statistics.StatisticsActor.PushTabSwitch
+
+class StatisticsActor extends Actor with ActorLogging {
+
+  // initialize a data structure for aggregating data across windows
+  val aggregationWindows: mutable.Map[Long, List[DataPoint]] = mutable.Map()
+
+  // initialize a queue where tab switches will be pushed for analysis
+  val tabSwitchQueue = mutable.Queue[(Tab, Tab)]()
+
+  override def preStart: Unit =
+    log.info("Starting to collect statistics")
+
+  override def receive: Actor.Receive = {
+    case PushTabSwitch(fromTab, toTab) =>
+      tabSwitchQueue.enqueue((fromTab, toTab))
+    case message => log.info(s"Received unknown message ${message.toString}")
+  }
+
+}
+
+object StatisticsActor {
+  case class PushTabSwitch(fromTab: Tab, toTab: Tab)
+}

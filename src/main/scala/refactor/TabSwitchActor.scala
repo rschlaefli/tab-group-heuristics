a0e1a6fcad40795f58630cb8919aa643963ac18d
Roland Schläfli <rolandschlaefli@gmail.com>
2020-06-19 15:15:11 +0200

    Log tab switches to CSV

--- src/main/scala/refactor/TabSwitchActor.scala
+++ src/main/scala/refactor/TabSwitchActor.scala
@@ -2,11 +2,25 @@ package refactor
 
 import akka.actor.Actor
 import akka.actor.ActorLogging
+import com.typesafe.scalalogging.LazyLogging
+import org.slf4j.MarkerFactory
 
 import tabstate.Tab
 
-class TabSwitchActor extends Actor with ActorLogging {
+class TabSwitchActor extends Actor with ActorLogging with LazyLogging {
+  import TabSwitchActor._
+
+  val logToCsv = MarkerFactory.getMarker("CSV")
+
   override def receive: Actor.Receive = {
+    case TabSwitch(Some(prevTab), newTab) => {
+      logger.info(
+        logToCsv,
+        s"${prevTab.id};${prevTab.hash};${prevTab.baseUrl};${prevTab.normalizedTitle};" +
+          s"${newTab.id};${newTab.hash};${newTab.baseUrl};${newTab.normalizedTitle}"
+      )
+    }
+
     case message => log.debug(s"Received message $message")
   }
 }

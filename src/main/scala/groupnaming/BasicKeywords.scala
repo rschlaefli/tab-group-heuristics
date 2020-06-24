32cfb93e33051a091983706ebd27de7ec9848ee4
Roland Schläfli <rolandschlaefli@gmail.com>
2020-06-24 19:34:09 +0200

    Remove keyword logging

--- src/main/scala/groupnaming/BasicKeywords.scala
+++ src/main/scala/groupnaming/BasicKeywords.scala
@@ -7,15 +7,12 @@ import tabswitches.TabMeta
 object BasicKeywords extends LazyLogging with NameGenerator {
 
   def apply(tabSet: Set[TabMeta]): (String, Set[TabMeta]) = {
-    logger.debug(s"Extracting keywords from set of tabs ${tabSet.toString()}")
 
     val allTitles = tabSet
       .map(deriveTabKeywordString)
       .mkString(" ")
-    logger.debug(s"Combined all titles in tab cluster: $allTitles")
 
     val keywords = extractKeywordsFromString(allTitles)
-    logger.debug(s"Extracted keywords from tab cluster: $keywords")
 
     (keywords.mkString(" "), tabSet)
   }
@@ -25,8 +22,6 @@ object BasicKeywords extends LazyLogging with NameGenerator {
     val tokenizedInput = normalizedInput.words("google")
     val frequencyMap = tokenizedInput.mkString(" ").bag("none")
     val sortedFrequencyMap = frequencyMap.toSeq.sortBy(_._2).reverse
-
-    logger.debug(s"Sorted frequency map ${sortedFrequencyMap.toString}")
 
     sortedFrequencyMap.take(4).map(_._1).toList
   }
